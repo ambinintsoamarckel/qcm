3b2686ec522deb6eafe56129ecaa9cc98b1a5111
mahm <marsonambinintsoa@gmail.com>
2024-09-03 19:45:18 +0300

    connection au base mongo

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -30,6 +30,18 @@ return [
     */
 
     'connections' => [
+    'mongodb' => [
+    'driver'   => 'mongodb',
+    'host'     => env('DB_HOST', '127.0.0.1'),
+    'port'     => env('DB_PORT', 27017),
+    'database' => env('DB_DATABASE', 'qcm'),
+    'username' => env('DB_USERNAME', ''),
+    'password' => env('DB_PASSWORD', ''),
+    'options'  => [
+        'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // This is where you specify the authentication database
+    ],
+],
+
 
         'sqlite' => [
             'driver' => 'sqlite',
